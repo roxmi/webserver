6083e661a7659cbbeeaa980c89e59b8f690faec5
Rosa <arduinoproyect@gmail.com>
2019-03-07 17:27:19 -0600

    Cambios de puerto

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers')
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 //Express HBS engine
